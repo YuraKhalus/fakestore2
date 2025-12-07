0459bf0b9db398cb05da77545e47383929021750
Roman <romantoporovych@gmail.com>
2025-12-07 15:27:21 +0200

    Styling card and add a picture

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,17 +18,17 @@ fetch(`${URL}/products`)
       console.log(listOfCategoryProducts);
       listOfCategoryProducts.forEach(product => {
          console.log(product);
-         products_box.innerHTML += `
-            <div class="product">
-               <img src="${product.image}" alt="" class="product_photo">
-               <div class="product_details">
-                  <h3 class="product_name">
-                     ${product.title}
-                  </h3>
-                  <span class="product_raiting">${product.rating.rate}</span>
-                  <h3 class="product_price">${product.price}$</h3>
-               </div>
-            </div>
-         `
+         // products_box.innerHTML += `
+         //    <div class="product">
+         //       <img src="${product.image}" alt="" class="product_photo">
+         //       <div class="product_details">
+         //          <h3 class="product_name">
+         //             ${product.title}
+         //          </h3>
+         //          <span class="product_raiting">${product.rating.rate}</span>
+         //          <h3 class="product_price">${product.price}$</h3>
+         //       </div>
+         //    </div>
+         // `
       });
    })
