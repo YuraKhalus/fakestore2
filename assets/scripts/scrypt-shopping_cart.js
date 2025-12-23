690702a782a1cd52f4454c5fe8c603aa1f774d8d
Roman <romantoporovych@gmail.com>
2025-12-23 18:46:59 +0200

    Add Quantity picker same as in Figma

--- assets/scripts/scrypt-shopping_cart.js
+++ assets/scripts/scrypt-shopping_cart.js
@@ -3,12 +3,12 @@
 const cards = document.querySelector('.cards');
 
 fetch(`https://fakestoreapi.com/products`)
-.then(respone => respone.json())
-.then(data => {
-    
-    const product = data[0];
-    console.log(product);
-    cards.innerHTML += `
+    .then(respone => respone.json())
+    .then(data => {
+
+        const product = data[0];
+        console.log(product);
+        cards.innerHTML += `
                 <div class="card">
                <div class="photo_block">
                   <img class="card_photo" src="${product.image}" alt="">
@@ -23,14 +23,13 @@ fetch(`https://fakestoreapi.com/products`)
                   </div>
                   
                   <div class="block_quantity">
-                     <input class="input_quantity" type="number" value="1" min="1" max="10">
+                     <div class="quantity-picker">
+                        <button class="quantity-button" id="quantity-minus">-</button>
+                        <p class="product-quantity">1</p>
+                        <button class="quantity-button" id="quantity-plus">+</button>
+                    </div>
+
                   </div>
-                     <!-- <div class="quantity">
-                        <button class="qty-btn minus">-</button>
-                        <span class="qty-value">01</span>
-                        <button class="qty-btn plus">+</button>
-                     </div>
-                  </div> -->
 
                   <div class="block_total">
                      <h3 class="total_card">$14.90</h3>
@@ -39,4 +38,36 @@ fetch(`https://fakestoreapi.com/products`)
 
             </div>
 `
+
+quantityCount();
 })
+
+
+
+
+
+
+function quantityCount() {
+
+    let quantityCounterMinus = document.getElementById('quantity-minus');
+    let quantityCounterPlus = document.getElementById('quantity-plus');
+    let productQuantity = document.querySelector('.product-quantity');
+    let quantity = 1;
+
+    quantityCounterPlus.addEventListener('click', () => {
+
+        
+        if (quantity < 10) {
+            productQuantity.innerText = ++quantity;
+        }
+        
+    })
+    quantityCounterMinus.addEventListener('click', () => {
+        if (quantity > 1) {
+            productQuantity.innerText = --quantity;
+        } 
+    })
+}
+
+
+
